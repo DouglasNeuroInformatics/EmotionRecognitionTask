01aee58e70fb7010477884f9fdd3f87eefdc8dc3
David Roper <davidroper321@gmail.com>
2024-07-30 14:54:18 -0400

    set up jsPsych tasks

--- src/EmotionRecognitionTask.ts
+++ src/EmotionRecognitionTask.ts
@@ -8,3 +8,39 @@ import audioButtonResponse from '@jspsych/plugin-audio-button-response';
 import audioKeyboardResponse from '@jspsych/plugin-audio-button-response';
 import VideoKeyboardResponsePlugin from "@jspsych/plugin-video-keyboard-response";
 
+
+const preload = {
+    type: PreloadPlugin,
+    auto_preload: true,
+    show_progress_bar: true,
+    message: `<p>loading media</p>`,
+};
+
+
+const audioCheck = {
+    type: audioKeyboardResponse,
+    stimulus: 'sound/tone.mp3',
+    prompt: "<p>Press any key to continue</p>",
+    response_ends_trial: true
+};
+
+const audioEmotionChoice = {
+    type: audioButtonResponse,
+    stimulus: 'sound/tone.mp3',
+    choices: ['Joy', 'Anger', 'Relief'],
+    prompt: "<p>Select the most accurate emotion </p>"
+}
+
+const videoCheck = {
+    type: VideoKeyboardResponsePlugin,
+    stimulus: 'video/video.mp4',
+    prompt: "<p>Press any key to continue</p>",
+    response_ends_trial: true
+}
+
+const videoEmotionChoice = {
+    type: videoButtonResponse,
+    stimulus: 'video/video.mp4',
+    choices: ['Joy', 'Anger', 'Relief'],
+    prompt: "<p> Select the most accurate emotion </p>"
+}
