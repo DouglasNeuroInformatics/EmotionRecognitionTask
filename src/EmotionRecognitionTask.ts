537447c66f8b7f9b0278e8cd54f72d080fde465a
David Roper <davidroper321@gmail.com>
2024-07-30 11:53:56 -0400

    add keyboard response plugins

--- src/EmotionRecognitionTask.ts
+++ src/EmotionRecognitionTask.ts
@@ -5,3 +5,6 @@ import SurveyHtmlFormPlugin from "@jspsych/plugin-survey-html-form";
 import videoButtonResponse from '@jspsych/plugin-video-button-response';
 import PreloadPlugin from "@jspsych/plugin-preload";
 import audioButtonResponse from '@jspsych/plugin-audio-button-response';
+import audioKeyboardResponse from '@jspsych/plugin-audio-button-response';
+import VideoKeyboardResponsePlugin from "@jspsych/plugin-video-keyboard-response";
+
