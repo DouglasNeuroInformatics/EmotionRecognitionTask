d513376f5cfced3b0a77bc2e5840f1df5022988b
David Roper <davidroper321@gmail.com>
2024-07-30 11:43:47 -0400

    added required jspsych modules

---
+++ src/EmotionRecognitionTask.ts
@@ -0,0 +1,6 @@
+import { initJsPsych } from "jspsych";
+import "jspsych/css/jspsych.css";
+import HtmlKeyboardResponsePlugin from "@jspsych/plugin-html-keyboard-response";
+import SurveyHtmlFormPlugin from "@jspsych/plugin-survey-html-form";
+import videoButtonResponse from '@jspsych/plugin-video-button-response';
+import PreloadPlugin from "@jspsych/plugin-preload";
