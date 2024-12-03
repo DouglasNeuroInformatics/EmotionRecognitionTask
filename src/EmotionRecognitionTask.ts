58e9831cbdf6e049cd20be05257c55e72bf7de86
David Roper <davidroper321@gmail.com>
2024-12-03 13:41:42 -0500

    replace repeated video cover html with variable, add continue button to audioEmotionChoice

--- src/EmotionRecognitionTask.ts
+++ src/EmotionRecognitionTask.ts
@@ -12,6 +12,60 @@ var jsPsych = initJsPsych();
 
 export default function emotionRecognitionTask() {
 
+  const VideoCoverHtml = `
+  <style>
+   /* Style for the video container */
+   .video-container {
+     position: relative;
+     width: 50vw;
+     height: 40vh;
+     overflow: hidden;
+   }
+
+   /* The black overlay */
+   .video-overlay {
+     position: absolute;
+     top: 0;
+     left: 0;
+     width: 100%;
+     height: 100%;
+     background-color: black;
+     color: white;
+     display: flex;
+     justify-content: center;
+     align-items: center;
+     cursor: pointer;
+     z-index: 2;
+     transition: .5s ease;
+   }
+
+   /* The video element */
+   video {
+     position: absolute;
+     object-fit: cover;
+     top: 0;
+     left: 0;
+     width: 100%;
+     height: 100%;
+   }
+   .spacedButtons {
+          padding: 10px
+        }
+ </style>
+ <div class="video-container">
+     <div class="video-overlay" id="overlay">
+       <svg id="overlay-cross" height="100" width="100" xmlns="http://www.w3.org/2000/svg">
+         <line x1="50" y1="0" x2="50" y2="100" style="stroke:white;stroke-width:18" />
+         <line x1="0" y1="50" x2="100" y2="50" style="stroke:white;stroke-width:18" />
+         Sorry, your browser does not support inline SVG.
+       </svg>
+     </div>
+     <video id="video" preload="auto" src="../video/Hello-There.mp4">
+     </video>
+     
+ </div>
+ `;
+
 
   const clickHandler = () => { document.addEventListener(
     "click",
@@ -54,7 +108,7 @@ export default function emotionRecognitionTask() {
     type: HtmlButtonResponse,
     stimulus: function() {
       let html = `
-      <svg id="audioIcon" xmlns="http://www.w3.org/2000/svg" style="align-content: center;" version="1.0" width="200" height="200" viewBox="0 0 45 120">
+      <svg id="audioIcon" xmlns="http://www.w3.org/2000/svg" style="align-content: center;" version="1.0" width="200" height="200" viewBox="10 0 45 120">
         <path d="M39.389,13.769 L22.235,28.606 L6,28.606 L6,47.699 L21.989,47.699 L39.389,62.75 L39.389,13.769z" style="stroke:#111;stroke-width:5;stroke-linejoin:round;fill:#111;"/>
         <path d="M48,27.6a19.5,19.5 0 0 1 0,21.4M55.1,20.5a30,30 0 0 1 0,35.6M61.6,14a38.8,38.8 0 0 1 0,48.6" style="fill:none;stroke:#111;stroke-width:5;stroke-linecap:round"/>
       </svg>
@@ -74,12 +128,20 @@ export default function emotionRecognitionTask() {
     
     response_allowed_while_playing: false,
     correct_answer: "Joy",
-    response_ends_trail: false,
+    response_ends_trial: false,
 
     on_load: () => {
+      addBootstrapScripts()
+
       let playOnce = false;
       const audioIcon = document.getElementById("audioIcon")
       const audioContent = document.getElementById("audioContent")
+
+      let response: string;
+      let start_time = 0;
+
+      let continueButton = addContinueButton()
+      document.body.appendChild(continueButton)
 
       if(audioIcon){
         audioIcon.addEventListener("click", () => {
@@ -94,10 +156,37 @@ export default function emotionRecognitionTask() {
       if(audioContent && audioContent instanceof HTMLAudioElement){
         audioContent.addEventListener('ended', () => {
           revealEmotionButtons()
+          continueButton.style.display = 'flex'
+
+
+          //set start time
+          start_time = performance.now();
         }
 
         )
       }
+
+      let buttonSelections = document.querySelectorAll('button[name="custom-button"]')
+
+      buttonSelections.forEach((button) => {
+        button.addEventListener('click', (e) => {
+          if(e.target === button){
+            let val = button.innerHTML;
+            response = val
+          }
+          
+        })
+      })
+
+      continueButton.addEventListener("click", (e) => {
+         
+        if(!response){
+          alert("Please select a button")
+          return
+        }
+        jsPsych.finishTrial({rt: (performance.now() - start_time), response: response});
+        continueButton.remove()
+      })
 
       
 
@@ -113,58 +202,8 @@ export default function emotionRecognitionTask() {
   const videoCheck = {
     type: HtmlKeyboardResponsePlugin,
     stimulus: function() {
-      
-      let html = `
-       <style>
-        /* Style for the video container */
-        .video-container {
-          position: relative;
-          width: 50vw;
-          height: 40vh;
-          overflow: hidden;
-        }
 
-        /* The black overlay */
-        .video-overlay {
-          position: absolute;
-          top: 0;
-          left: 0;
-          width: 100%;
-          height: 100%;
-          background-color: black;
-          color: white;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          cursor: pointer;
-          z-index: 2;
-          transition: .5s ease;
-        }
-
-        /* The video element */
-        video {
-          position: absolute;
-          object-fit: cover;
-          top: 0;
-          left: 0;
-          width: 100%;
-          height: 100%;
-        }
-      </style>
-      <div class="video-container">
-          <div class="video-overlay" id="overlay">
-            <svg id="overlay-cross" height="100" width="100" xmlns="http://www.w3.org/2000/svg">
-              <line x1="50" y1="0" x2="50" y2="100" style="stroke:white;stroke-width:18" />
-              <line x1="0" y1="50" x2="100" y2="50" style="stroke:white;stroke-width:18" />
-              Sorry, your browser does not support inline SVG.
-            </svg>
-          </div>
-          <video id="video" preload="auto" src="../video/Hello-There.mp4">
-          </video>
-          
-      </div>
-      `;
-      return html
+      return VideoCoverHtml
     },
     prompt: "<p>Press any key to continue after video has completed</p>",
     response_ends_trial: true,
@@ -226,61 +265,7 @@ export default function emotionRecognitionTask() {
     type: HtmlButtonResponse,
     stimulus: function() {
       
-      let html = `
-      <style>
-       /* Style for the video container */
-       .video-container {
-         position: relative;
-         width: 50vw;
-         height: 40vh;
-         overflow: hidden;
-       }
-
-       /* The black overlay */
-       .video-overlay {
-         position: absolute;
-         top: 0;
-         left: 0;
-         width: 100%;
-         height: 100%;
-         background-color: black;
-         color: white;
-         display: flex;
-         justify-content: center;
-         align-items: center;
-         cursor: pointer;
-         z-index: 2;
-         transition: .5s ease;
-       }
-
-       /* The video element */
-       video {
-         position: absolute;
-         object-fit: cover;
-         top: 0;
-         left: 0;
-         width: 100%;
-         height: 100%;
-       }
-     </style>
-     <div class="video-container">
-         <div class="video-overlay" id="overlay">
-           <svg id="overlay-cross" height="100" width="100" xmlns="http://www.w3.org/2000/svg">
-             <line x1="50" y1="0" x2="50" y2="100" style="stroke:white;stroke-width:18" />
-             <line x1="0" y1="50" x2="100" y2="50" style="stroke:white;stroke-width:18" />
-             Sorry, your browser does not support inline SVG.
-           </svg>
-         </div>
-         <video id="video" preload="auto" src="../video/Hello-There.mp4">
-         </video>
-
-         .spacedButtons {
-          padding: 10px
-        }
-         
-     </div>
-     `;
-      return html
+      return VideoCoverHtml
     },
     choices: ["Joy", "Anger", "Madness"],
     button_html: (choice: string) => {
@@ -347,30 +332,31 @@ export default function emotionRecognitionTask() {
         start_time = performance.now();
 
 
-       });
-      } 
-     }
-        let buttonSelections = document.querySelectorAll('button[name="custom-button"]')
+        });
+       } 
+      }
 
-        buttonSelections.forEach((button) => {
-          button.addEventListener('click', (e) => {
-            if(e.target === button){
-              let val = button.innerHTML;
-              response = val
-            }
-            
-          })
-        })
+      let buttonSelections = document.querySelectorAll('button[name="custom-button"]')
 
-        continueButton.addEventListener("click", (e) => {
-         
-           if(!response){
-            alert("Please select a button")
-            return
+      buttonSelections.forEach((button) => {
+        button.addEventListener('click', (e) => {
+          if(e.target === button){
+            let val = button.innerHTML;
+            response = val
           }
-          jsPsych.finishTrial({rt: (performance.now() - start_time), response: response});
-          continueButton.remove()
+          
         })
+      })
+
+      continueButton.addEventListener("click", (e) => {
+         
+        if(!response){
+          alert("Please select a button")
+          return
+        }
+        jsPsych.finishTrial({rt: (performance.now() - start_time), response: response});
+        continueButton.remove()
+      })
     },
   };
 
@@ -378,10 +364,10 @@ export default function emotionRecognitionTask() {
   timeline.push(preload);
   timeline.push(instructions);
   timeline.push(audioHtmlEmotionChoice)
-  // timeline.push(audioCheck);
-  // timeline.push(videoInstructions);
-  // timeline.push(videoCheck);
-  // timeline.push(videoCheckWithButtons);
+  timeline.push(audioCheck);
+  timeline.push(videoInstructions);
+  timeline.push(videoCheck);
+  timeline.push(videoCheckWithButtons);
   jsPsych.run(timeline);
 
   const addBootstrapScripts = () => {
