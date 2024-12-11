e50ceb951fee42e60800d01aa1b50f00876ad5ca
David Roper <davidroper321@gmail.com>
2024-12-11 14:47:16 -0500

    update i18n.ts page to latest set up

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,58 +1,23 @@
-import { createI18Next } from "/runtime/v1/@opendatacapture/runtime-core";
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
+/* eslint-disable @typescript-eslint/no-namespace */
 
-document.documentElement.setAttribute("lang", "en");
+import { i18n } from '@douglasneuroinformatics/libui/i18n';
 
-const i18n = createI18Next({
+
+declare module '@douglasneuroinformatics/libui/i18n' {
+  export namespace UserConfig {
+    export interface LanguageOptions {
+      en: true;
+      fr: true;
+    }
+    export interface Translations {
+    }
+  }
+}
+
+i18n.init({
   translations: {
-    welcome: {
-      en: "Welcome. Press any key to begin",
-      fr: "Bienvenue. Appuyez sur n'importe quelle touche pour commencer",
-    },
-    correctResponse: {
-      en: "Correct response:",
-      fr: "La réponse correcte est:",
-    },
-    loadingStimulus: {
-      en: "Loading stimulus",
-      fr: "Stimulus de chargement",
-    },
-    logResponse: {
-      en: "Log the response",
-      fr: "Enregistrer la réponse",
-    },
-    correct: {
-      en: "Correct",
-      fr: "Correct",
-    },
-    incorrect: {
-      en: "Incorrect",
-      fr: "Incorrect",
-    },
-    responseWas: {
-      en: "The response was:",
-      fr: "La réponse a été donnée:",
-    },
-    logNotes: {
-      en: "Log any other notes here",
-      fr: "Inscrivez ici toute autre note",
-    },
-    logResponseToContinue: {
-      en: "Log response and press submit to continue",
-      fr: "Enregistrez la réponse et appuyez sur 'soumettre' pour continuer",
-    },
-    submit: {
-      en: "Submit",
-      fr: "Soumettre",
-    },
-    continue: {
-      en: "Continue",
-      fr: "Continuer"
-    },
-  },
+  }
 });
-// the whole i18n implementation needs to be changed
-document.addEventListener("changeLanguage", function (event) {
-  // @ts-expect-error the event does have a detail
-  document.documentElement.setAttribute("lang", event.detail as string);
-});
+
 export default i18n;
