7d49c8bde380c7fe4a171b29dad9e1455864749e
David Roper <davidroper321@gmail.com>
2025-01-15 15:19:12 -0500

    add new ts configuration file for ODC impletemented version

---
+++ src/ODCMediaContent.ts
@@ -0,0 +1,20 @@
+import ang_Gemep06 from './06ang_Gemep.mp4'
+import joy_Gemep07 from './07joy_Gemep.mp4'
+
+export const testContent = {
+  "Content": {
+    "Video": {
+      "06col313": {
+        "Emotions": ["Anger", "Fear", "Contempt"],
+        "CorrectAnswer": "Anger",
+        "Filepath": ang_Gemep06
+      },
+      "07joi114": {
+        "Emotions": ["Interest", "Joy", "Pride"],
+        "CorrectAnswer": "Joy",
+        "Filepath": joy_Gemep07
+      },
+    }
+    
+  }
+}
