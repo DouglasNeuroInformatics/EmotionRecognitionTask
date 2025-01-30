a2c538a661dfdf999d6024e07949dd4394e2d276
David Roper <davidroper321@gmail.com>
2025-01-30 16:18:46 -0500

    initialize translator in index.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import './style.css';
 import emotionRecognitionTask from './EmotionRecognitionTask.ts';
+import { experimentSettingsJson } from "./experimentSettings.ts";
 import '/runtime/v1/jspsych@8.x/css/jspsych.css';
 const { defineInstrument } = await import('/runtime/v1/@opendatacapture/runtime-core/index.js');
 const { z } = await import('/runtime/v1/zod@3.23.x/index.js');
-import { $EmotionRecognitionTaskResult } from './schemas.ts';
+import { $EmotionRecognitionTaskResult, $Settings } from './schemas.ts';
+import { translator } from './translations.ts';
+
+import type { Language } from "/runtime/v1/@opendatacapture/runtime-core";
 
 export default defineInstrument({
   kind: "INTERACTIVE",
-  language: 'en',
+    // if multilingual experimentSettingsJson needs a language field
+  language: experimentSettingsJson.language as Language,
   tags: ["interactive", "jsPysch", "PictureNamingTask"],
 
 
@@ -17,6 +22,14 @@ export default defineInstrument({
   },
   content: {
     render() {
+      const settingsParseResult = $Settings.safeParse(experimentSettingsJson);
+
+      // parse settings
+      if (!settingsParseResult.success) {
+        throw new Error(`Validation error, check experiment settings: ${settingsParseResult.error.toString()}`)
+      }
+      // translator.init();
+      translator.changeLanguage(settingsParseResult.data.language as Language);
       emotionRecognitionTask();
     }
   },
