e20865d7fbc63821dea4ba1711c96ad24961209b
david-roper <davidroper321@gmail.com>
2025-01-31 15:36:31 -0500

    edit svg sizing

--- src/helperFunctions.ts
+++ src/helperFunctions.ts
@@ -78,7 +78,7 @@ export const videoCoverHtmlGenerator = (filepath: string, top: string = '40%', l
 
 export const audioHtmlGenerator = (filepath: string) => {
   return ` 
-      <svg id="audioIcon" xmlns="http://www.w3.org/2000/svg" style="align-content: center;" version="1.0" width="200" height="200" viewBox="10 0 45 120">
+      <svg id="audioIcon" xmlns="http://www.w3.org/2000/svg" style="align-content: center;" version="1.0" width="200" height="180" viewBox="15 -12 40 100">
             <path d="M39.389,13.769 L22.235,28.606 L6,28.606 L6,47.699 L21.989,47.699 L39.389,62.75 L39.389,13.769z" style="stroke:#111;stroke-width:5;stroke-linejoin:round;fill:#111;"/>
             <path d="M48,27.6a19.5,19.5 0 0 1 0,21.4M55.1,20.5a30,30 0 0 1 0,35.6M61.6,14a38.8,38.8 0 0 1 0,48.6" style="fill:none;stroke:#111;stroke-width:5;stroke-linecap:round"/>
         </svg>
