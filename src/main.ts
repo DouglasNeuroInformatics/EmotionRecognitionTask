9eaabc369bf6ca1ee6fdde32377e5d795b9b21c7
David Roper <davidroper321@gmail.com>
2025-03-26 16:29:01 -0400

    remove googleDriveVideo component

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import './index.css';
 import emotionRecognitionTask from './EmotionRecognitionTask';
 import '/runtime/v1/jspsych@8.x/css/jspsych.css';
-import { googleDriveVideo } from './testGoogleDriveVideo';
 
 
-//googleDriveVideo()
 await emotionRecognitionTask();
