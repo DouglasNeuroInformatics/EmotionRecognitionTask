4132382c2c8b07974eddde8bc9bbd3761b2680e4
David Roper <davidroper321@gmail.com>
2025-05-29 16:47:06 -0400

    feat: remove addnotification from main

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,7 @@ import emotionRecognitionTask from './EmotionRecognitionTask';
 import '/runtime/v1/jspsych@8.x/css/jspsych.css';
 
 import { configureNotifications } from './notifications';
-import { addNotification } from '@opendatacapture/runtime-v1/@opendatacapture/runtime-core/index.js';
 
 configureNotifications();
-
-setTimeout(() => {
-  addNotification({
-    type: 'info',
-    message: 'Welcome!'
-  });
-}, 500);
 
 await emotionRecognitionTask();
